5102a464c9a25c2ad55f0620f6e34b8038f82b5c
Craig Weston <craig.weston1@hotmail.co.uk>
2018-02-15 16:54:02 +0000

    Made corrections and updated for 'Extra Credit'

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() { 
+	
 	var checked = $("checkbox:checked");
 	var all = $("input[name='all'");
 	var frameworks = $("input[name='js-frameworks'");
@@ -7,6 +8,7 @@ $(document).ready(function() {
 	var node = $("input[name='node']");
 	var build = $("input[name='build-tools']");
 	var npm = $("input[name='npm']");
+	var error =  $('.error-message');
 	
 	$('#name').focus();
 	$('#other-title').hide();
@@ -14,6 +16,28 @@ $(document).ready(function() {
 	$('#paypal').hide();
 	$('#card').hide();
 	$('#card-number').hide();
+	
+	//////////////////////////////////////////////////////////////////////
+	// Form Validation Variables
+	//////////////////////////////////////////////////////////////////////
+	  // Name Validation
+	  var nameValid = false;
+	  //Email Validation
+	  var emailValid = false;
+	  // Role/Job title initial state validation variable
+	  var tShirtSelected = false;
+	  // Payment initial state of form validation variable
+	  var ccNumValid = false;
+	  var zipValid = false;
+	  var cValidate = false;
+	  var paymentValid = false;
+	
+	///////////////////////////////////////////////////////////////////////////
+	// Payment Fieldset Validator
+	//////////////////////////////////////////////////////////////////////////
+	
+	// Set CVV max length
+	$('#cvv').attr('maxlength', 3);
 
 	$('#title').change(function() {
 			
@@ -25,27 +49,127 @@ $(document).ready(function() {
 		}
 	});
 	
-	$('#design').change(function() {
+	//////////////////////////////////////////////////////////////////////////
+	// Form Validation
+	//////////////////////////////////////////////////////////////////////////
+	
+	
+	
+	$("#name").keyup(function() {
+	  if ($('#name').val() != '') {
+		nameValid = true;
+		return nameValid;
+	  }
+	});
+	
+
+	var validateEmail = function(email) {
+		var RegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return RegEx.test(email);
+	};
+	
+	// On KeyUp, check if email is valid
+	$("#mail").keyup(function() {
+	  emailValid = validateEmail($(this).val());
+	  return emailValid;
+	});
+	
+	
+	$("#zip").keyup(function() {
+	  if ($('#zip').val().length === 5) {
+		zipValid = true;
+		return zipValid;
+	  }
+	});
+	
+	
+	$("#cvv").keyup(function() {
+	  if ($('#cvv').val().length === 3) {
+		ccNumValid = true;
+		return ccNumValid;
+	  }
+	});
+	
+	
+	$("#cc-num").keyup(function() {
+	  if ($('#cc-num').val().length > 12 && $('#cc-num').val().length < 17) {
+		ccNumValid = true;
+		return ccNumValid;
+	  }
+	});
+
+	$('form').submit(function(e){
 		
-		if (this.value == "js puns") {
-			// display "Cornflower Blue," "Dark Slate Grey," and "Gold." in color menu
-			$(".love").hide();
-			$(".puns").show();
+		if (nameValid != true) {
+		 $('label[for="name"]').css("color", "red");
+			$('#name').attr("placeholder", "Please enter your name");
+			e.preventDefault();
 		}
-		else if (this.value == "heart js") {
-			// display "Tomato," "Steel Blue," and "Dim Grey."
-			$(".puns").hide();
-			$(".love").show();
+		
+
+		if (emailValid != true) {
+			$('label[for="mail"]').css("color", "red");
+			$('#mail').attr("placeholder", "Please enter your email");
+			e.preventDefault();
 		}
-		else {
-			$(".puns").show();
-			$(".love").show();
+		
+		if (cValidate != true) {
+			$('label[for="cvv"]').css("color", "red");
+			$('#cvv').attr("placeholder", "Please enter your 3 digit CVV");
+			e.preventDefault();
+		}
+
+		if (ccNumValid != true) {
+			$('label[for="cc-num"]').css("color", "red");
+			$('#cc-num').attr("placeholder", "Please enter a number that is between 13 and 16 digits long");
+			e.preventDefault();
+		}
+		
+		if (zipValid != true) {
+			$('label[for="zip"]').css("color", "red");
+			$('#zip').attr("placeholder", "Please enter your 5 digit ZIP");
+			e.preventDefault();
+		}
+		
+
+		if ($('.activities :checkbox:checked').length == 0){
+			$('#activity-error').append('<h3 style="color:red">Please select your activity below:</h3>');
+			e.preventDefault();
+		}
+
+		else if (nameValid = true){
+			$('label[for="name"]').css("color", "green");
+			$('#activity-error').hide();
 		}
 		
 	});
-
 	
-	// If  If the user selects a workshop, don't allow selection of a workshop at the same date and time -- disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
+	
+  // Hide Color select until a design is selected
+  $('#colors-js-puns').hide();
+	
+// Show design colours on change
+  var shirtSelected = false;
+  $('#design').change(function(){
+  	if ($('#design option:selected').val() === "js puns") {
+  		$('#colors-js-puns').show();
+  		$('#color').html('<option value="cornflowerblue">Cornflower Blue (JS Puns shirt only)</option><option value="darkslategrey">Dark Slate Grey (JS Puns shirt only)</option><option value="gold">Gold (JS Puns shirt only)</option>');
+  		shirtSelected = true;
+  		return shirtSelected;
+  	} else if ($('#design option:selected').val() === "heart js") {
+  		$('#colors-js-puns').show();
+  		$('#color').html('<option value="tomato">Tomato (I &#9829; JS shirt only)</option><option value="steelblue">Steel Blue (I &#9829; JS shirt only)</option><option value="dimgrey">Dim Grey (I &#9829; JS shirt only)</option>');
+  		shirtSelected = true;
+  		return shirtSelected;
+  	} else {
+  		$('#colors-js-puns').hide();
+  		shirtSelected = false;
+  		return shirtSelected;
+  	}
+  });
+	
+
+	// If the user selects a workshop, don't allow selection of a workshop at the same date and time -- disable the checkbox and visually indicate that the workshop in the competing time slot isn't available.
 	
 	$(frameworks).change(function(){
 		$(express).attr('disabled', this.checked);
@@ -89,7 +213,6 @@ $(document).ready(function() {
 		
 	});
 	
-	
 	$(all).change(function(){
 		if (all.is(':checked')) {
 		updateCost(200);
@@ -99,89 +222,50 @@ $(document).ready(function() {
 		
 	});
 	
-	
 	$(build).change(function(){
 		if (build.is(':checked')) {
 		updateCost(100);
 		} else{
 		updateCost(-100);
 		}
-		
 	});
 	
 	
-	
 	$(npm).change(function(){
-
 		if (npm.is(':checked')) {
 		updateCost(100);
 		} else{
 		updateCost(-100);
 		}
-		
 	});
-	
-	//Must select at least one checkbox under the "Register for Activities" section of the form.
- 	$('button').click(function() {
-      checked = $("input[type=checkbox]:checked").length;
-
-      if(!checked) {
-        alert("You must check at least one checkbox.");
-        return false;
-      }
-	  
-	  	if ($('#zip').val().length !== 5){
-			alert('Please enter a 5-digit number for the ZIP code');
-		}
-		
-		if ($('#zip').val().length !== 3){
-			alert('Please enter a 3-digit number for the CVV value');
-		}
-	});
-	
-	$(document).keypress(function(event) {
-
-		 if ($('#cc-num').val().length < 12){
-			$('#card-number').show();
-			}
-			else if ($('#cc-num').val().length > 17){
-			$('#card-number').slideDown();
-			}
-			
-			else {
-				$('#card').slideUp();
-			$('#card-number').slideUp();
-			}
-			
-    });
 	
 	// As a user selects activities, a running total displays below the list of checkboxes.
+	
 	var totalCost = 0;
 	$('.activities').append('<label>Total:</label><div id="total"></div>');
 	
 	var updateCost = function (cost) {
 		totalCost += cost;
 		document.getElementById("total").innerHTML = "Total Costs: $" + totalCost;
-	};  
+	}; 
+	
+	// Display relevant message depending on payment type
 	
 	$('#payment').change(function() {
 	  if(this.value === "paypal") {
-	   
 		$('#paypal').slideDown();
 		$('#bitcoin').slideUp();
-	   
+		  $('#credit-card').slideUp();
+		  
 	} else if(this.value === "bitcoin") {
-		
 		$('#bitcoin').slideDown();
 		$('#paypal').slideUp();
+		$('#credit-card').slideUp();
 	}
 	else {
 		$('#bitcoin').hide();
 		$('#paypal').hide();
+		$('#credit-card').slideDown();
 		}
-		
 	});
-
-	
-	
 });
